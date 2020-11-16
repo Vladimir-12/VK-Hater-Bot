b12cec73492782f70344c5b2ebbcc3dc2d837c05
Wolf In Chains <47662957+WolfInChains@users.noreply.github.com>
2020-11-16 12:55:20 +0200

    Update bot_user.py

--- bot_user.py
+++ bot_user.py
@@ -1,43 +1,14 @@
+import time
 import vk_api
 import random
-import config
+import config as c
+from modules import sqlite_methods as m
+from threading import Thread
 from vk_api.longpoll import VkLongPoll, VkEventType
-import sqlite3
 
-admins = []
-bot_id = 
-token = ''
-
-conn = sqlite3.connect("db.db")
-c = conn.cursor()
-
-vk_session = vk_api.VkApi(token=token)
+vk_session = vk_api.VkApi(token=c.u_token)
 longpoll = VkLongPoll(vk_session)
 vk = vk_session.get_api()
-
-
-def is_user_hatelisted(user_id: int) -> bool:
-    c.execute("SELECT count(user_id) FROM users WHERE user_id = %d" % user_id)
-    result = c.fetchone()[0]
-    return result > 0
-
-
-def insert_hatelist(user_id: int):
-    c.execute("INSERT INTO users(user_id) VALUES (%d)" % user_id)
-    conn.commit()
-    print(f"Пользователь {user_id} добавлен в хейтлист")
-
-
-def delete_hatelist(user_id: int):
-    c.execute("DELETE FROM users WHERE user_id=%d" % user_id)
-    conn.commit()
-    print(f"Пользователь {user_id} удален из хейтлиста")
-
-
-def get_all_users():
-    c.execute("SELECT * FROM users")
-    result = c.fetchall()
-    return result
 
 
 def send_msg_reply(reply_to: int, peer_id: int, message: str, attachment: str = ""):
@@ -50,65 +21,100 @@ def send_msg(peer_id: int, message: str, attachment: str = ""):
 
 user_ids = []
 
-users = get_all_users()
+users = m.get_all_users()
 for user in users:
     user_ids.append(user[0])
 
-random.shuffle(config.VARIANTS)
+settings_result = []
 
-index = 0
+settings = m.get_settings(1)
+for value in settings:
+    settings_result.append(value)
 
-while True:
-    try:
-        for event in longpoll.listen():
-            if event.type == VkEventType.MESSAGE_NEW:
+random.shuffle(c.VARIANTS)
 
-                if event.user_id in user_ids:
-                    if not event.from_me:
-                        if index < len(config.VARIANTS):
-                            send_msg_reply(event.message_id, event.peer_id, f'{config.VARIANTS[index]}')
-                            index += 1
-                        else:
-                            index = 0
-                            random.shuffle(config.VARIANTS)
-                            send_msg_reply(event.message_id, event.peer_id, f'{config.VARIANTS[index]}')
-                            index += 1
 
-                if event.text == "+хейт":
-                    if event.user_id in admins:
-                        msg = vk.messages.getById(message_ids=event.message_id)['items'][0]
-                        if 'reply_message' in msg:
-                            user = msg['reply_message']['from_id']
-                            if user != bot_id:
-                                if not is_user_hatelisted(user):
-                                    user_ids.append(user)
-                                    insert_hatelist(user)
-                                    send_msg(event.peer_id, '✅ Пользователь добавлен в хейт лист')
+def hate():
+    index = 0
+    while True:
+        try:
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW:
+
+                    if event.user_id in user_ids:
+                        if not event.from_me:
+                            if index < len(c.VARIANTS):
+                                time.sleep(settings_result[1])
+                                send_msg_reply(event.message_id, event.peer_id, f'{c.VARIANTS[index]}')
+                                index += 1
+                            else:
+                                index = 0
+                                random.shuffle(c.VARIANTS)
+                                time.sleep(settings_result[1])
+                                send_msg_reply(event.message_id, event.peer_id, f'{c.VARIANTS[index]}')
+                                index += 1
+        except Exception as e:
+            print(repr(e))
+
+
+def commands():
+    while True:
+        try:
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW:
+
+                    text = event.text
+                    split_text = event.text.split(' ')
+
+                    if text in c.hate:
+                        if event.user_id in c.u_admins:
+                            msg = vk.messages.getById(message_ids=event.message_id)['items'][0]
+                            if 'reply_message' in msg:
+                                user = msg['reply_message']['from_id']
+                                if user != c.bot_id:
+                                    if not m.is_user_hatelisted(user):
+                                        user_ids.append(user)
+                                        m.insert_hatelist(user)
+                                        send_msg(event.peer_id, '✅ Пользователь добавлен в хейт лист')
+                                    else:
+                                        send_msg(event.peer_id, '❎ Пользователь уже находится в хейт листе')
                                 else:
-                                    send_msg(event.peer_id, '❎ Пользователь уже находится в хейт листе')
+                                    send_msg(event.peer_id, '❎ Невозможно добавить бота в хейт лист')
                             else:
-                                send_msg(event.peer_id, '❎ Невозможно добавить бота в хейт лист')
+                                send_msg(event.peer_id, '❎ Пользователь не указан')
                         else:
-                            send_msg(event.peer_id, '❎ Пользователь не указан')
-                    else:
-                        choice = random.choice(config.ERRORS)
-                        send_msg(event.peer_id, f'{choice}')
+                            choice = random.choice(c.ERRORS)
+                            send_msg(event.peer_id, f'{choice}')
 
-                if event.text == "-хейт":
-                    if event.user_id in admins:
-                        msg = vk.messages.getById(message_ids=event.message_id)['items'][0]
-                        if 'reply_message' in msg:
-                            user = msg['reply_message']['from_id']
-                            if is_user_hatelisted(user):
-                                user_ids.remove(user)
-                                delete_hatelist(user)
-                                send_msg(event.peer_id, '✅ Пользователь удален из хейт листа')
+                    if text in c.unhate:
+                        if event.user_id in c.u_admins:
+                            msg = vk.messages.getById(message_ids=event.message_id)['items'][0]
+                            if 'reply_message' in msg:
+                                user = msg['reply_message']['from_id']
+                                if m.is_user_hatelisted(user):
+                                    user_ids.remove(user)
+                                    m.delete_hatelist(user)
+                                    send_msg(event.peer_id, '✅ Пользователь удален из хейт листа')
+                                else:
+                                    send_msg(event.peer_id, '❎ Пользователь отсутствует в хейт листе')
                             else:
-                                send_msg(event.peer_id, '❎ Пользователя отсутствует в хейт листе')
+                                send_msg(event.peer_id, '❎ Пользователь не указан')
                         else:
-                            send_msg(event.peer_id, '❎ Пользователь не указан')
-                    else:
-                        choice = random.choice(config.ERRORS)
-                        send_msg(event.peer_id, f'{choice}')
-    except Exception as e:
-        print(repr(e))
+                            choice = random.choice(c.ERRORS)
+                            send_msg(event.peer_id, f'{choice}')
+
+                    if split_text[0] in c.cooldown:
+                        if len(split_text) == 2 and split_text[1].isnumeric():
+                            settings_result.pop(1)
+                            settings_result.insert(1, int(split_text[1]))
+                            m.set_cooldown(int(split_text[1]), 1)
+                            send_msg(event.peer_id, f'✅ Задержка изменена на {split_text[1]} секунд')
+                        else:
+                            send_msg(event.peer_id, '❎ Неверный формат команды')
+        except Exception as e:
+            print(repr(e))
+
+
+if __name__ == '__main__':
+    Thread(target=hate, args=[]).start()
+    Thread(target=commands, args=[]).start()
